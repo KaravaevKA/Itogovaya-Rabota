bc14b25cb78bc0fc7a1a5ecfbbda868339ca0c68
Kirill Karavaev <coffee.2016@yandex.ru>
2023-02-14 18:35:32 +0300

    Добавлена вторая версия задания

--- Zadanie/Program.cs
+++ Zadanie/Program.cs
@@ -1,6 +1,10 @@
 ﻿
-string[] Array = new string[3]{"Hello", "World", "23"};
-
+string[] Array = new string[3];
+for (int i = 0; i <Array.Length; i++)
+{   
+    Console.Write($"Введите {i+1} элемент массива: ");
+    Array[i] = Console.ReadLine();
+}
 for (int i = 0; i <Array.Length; i++)
 {   
     if(Array[i].Length <= 3) Console.WriteLine(Array[i]);
